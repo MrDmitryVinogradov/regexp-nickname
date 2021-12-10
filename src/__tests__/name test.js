d67986528e9a253dac013e3cddb225d5c77acb7e
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-12-10 15:23:25 +0300

    fix of tests

--- src/__tests__/name test.js
+++ src/__tests__/name test.js
@@ -1,19 +1,23 @@
 import Validator from '../js/classes/Validator';
 
 test('wrong first sym', () => {
-    expect(() => new Validator('-foo').validateUsername().toEqual(false));
+  expect(new Validator('-foo').validateUsername()).toEqual(false);
 })
 
 test('too many digits', () => {
-  expect(() => new Validator('user12345').validateUsername().toEqual(false));
+  expect(new Validator('user12345').validateUsername()).toEqual(false);
 })
 
 test('wrong last sym', () => {
-  expect(() => new Validator('bar-').validateUsername().toEqual(false));
+  expect(new Validator('bar-').validateUsername()).toEqual(false);
+})
+
+test('wrong sym', () => {
+  expect(new Validator('User+user').validateUsername()).toEqual(false);
 })
 
 test('ok name', () => {
-  expect(() => new Validator('User123ok').validateUsername().toEqual(true));
+  expect(new Validator('User123ok').validateUsername()).toEqual(true);
 })
 
 
